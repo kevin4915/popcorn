776ae4c385f3d006879c835a1029cd2e23fad5aa
Kévin Delassat <kevin.delassat@gmail.com>
2026-03-09 18:13:07 +0100

    Update pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
   def home
     @top_week = Movie.order(rating: :desc).limit(10)
     @top_personal = Movie.joins(:historics).where(historics: { user_id: current_user.id }).order(rating: :desc).limit(10)
