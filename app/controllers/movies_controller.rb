e6489884e34af207aaa2b2ca7cfd3a9f694ed6ee
antoine rodrigues <antoine2000r@hotmail.fr>
2026-03-10 10:50:49 +0100

    pageselection

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,13 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all
   end
 
-  def show
+  def swipe
+    @movie = Movie.find(params[:id])
+    if params[:decision] == "like"
+      Historic.create!(user: current_user, movie: @movie)
+    end
+    head :ok
   end
 end
