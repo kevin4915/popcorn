a33eedf2d3a30d59c0a5b5dccaf54cf31ea4dc0a
nabilazoula <nabil.azoula@gmail.com>
2026-03-10 11:27:35 +0100

    cards 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,11 @@
 class MoviesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def index
+    @movies = Movie.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,52 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Historic.destroy_all
+Review.destroy_all
+Movie.destroy_all
+Platform.destroy_all
+User.destroy_all
+
+puts "Creating platforms..."
+netflix = Platform.create!(name: "Netflix")
+prime = Platform.create!(name: "Prime Video")
+disney = Platform.create!(name: "Disney+")
+
+puts "Creating movies..."
+movie1 = Movie.create!(
+  title: "Inception",
+  synopsis: "Un voleur infiltre les rêves pour voler des secrets.",
+  year: 2010,
+  duration: 148,
+  rating: 8.8,
+  category: "Science-fiction",
+  platform: netflix,
+  director: "Christopher Nolan",
+  poster_url: "inception.jpg",
+  trailer: "https://www.youtube.com/watch?v=YoHD9XEInc0"
+)
+
+movie2 = Movie.create!(
+  title: "Interstellar",
+  synopsis: "Un groupe d'explorateurs voyage à travers un trou de ver.",
+  year: 2014,
+  duration: 169,
+  rating: 8.6,
+  category: "Science-fiction",
+  platform: prime,
+  director: "Christopher Nolan",
+  poster_url: "interstellar.jpg",
+  trailer: "https://www.youtube.com/watch?v=zSWdZVtXT7E"
+)
+
+puts "Creating reviews..."
+Review.create!(movie: movie1, rating: 9, comment: "Chef d'œuvre visuel.")
+Review.create!(movie: movie1, rating: 8, comment: "Très intelligent et captivant.")
+Review.create!(movie: movie2, rating: 10, comment: "Magnifique et émouvant.")
+
+puts "Creating historic..."
+Historic.create!(user: user, movie: movie1, watched_at: Time.now)
+Historic.create!(user: user, movie: movie2, watched_at: Time.now)
+
+puts "Done!"
