b8bcf3039fff24c381f03677246afc03e8eee96d
guillaume.delattre.it@gmail.com <guillaume.delattre.it@gmail.com>
2026-03-11 14:30:48 +0100

    Revue API + home show swipe

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,19 +9,13 @@ class MoviesController < ApplicationController
     "HBO" => 384
   }.freeze
 
-  def show
-    @movie  = Movie.find(params[:id])
-    credits = tmdb_get("movie/#{@movie.tmdb_id}/credits", language: "fr-FR")
-    @actors = credits["cast"]&.first(4) || []
-  end
-
   def index
     genre_id          = tmdb_genre_id(params[:genre])
     type              = params[:serie] == "1" ? "tv" : "movie"
     max_duration      = parse_duration(params[:duration], type)
     user_provider_ids = user_platforms
     without_genres    = excluded_genres(params[:company])
-    seen_tmdb_ids     = current_user.movies.pluck(:tmdb_id).compact
+    seen_tmdb_ids     = current_user&.movies&.pluck(:tmdb_id)&.compact || []
     sort_options      = %w[popularity.desc vote_count.desc revenue.desc primary_release_date.desc]
 
     tmdb_query = {
@@ -29,11 +23,15 @@ class MoviesController < ApplicationController
       with_genres: genre_id,
       without_genres: without_genres,
       sort_by: sort_options.sample,
-      with_watch_providers: user_provider_ids,
       watch_region: "FR",
       "vote_count.gte" => 20,
       page: rand(1..20)
     }
+
+    if user_provider_ids.present?
+      tmdb_query[:with_watch_providers] = user_provider_ids
+      tmdb_query[:with_watch_monetization_types] = "flatrate"
+    end
 
     if max_duration
       tmdb_query["with_runtime.lte"] = max_duration
@@ -50,9 +48,21 @@ class MoviesController < ApplicationController
       )
     end
 
-    @movies = tmdb_results.first(10).shuffle.map { |result| upsert_movie(result) }
+    @movies = tmdb_results.first(10).shuffle.map { |result| upsert_movie(result, type) }
 
     render :swipe
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+
+    if @movie.actors.blank? || @movie.trailer.blank? || @movie.duration.blank? || @movie.platform.blank?
+      enrich_movie_from_tmdb!(@movie)
+      @movie.save!
+    end
+
+    @actors = parse_actors(@movie.actors)
+    @platforms = parse_platforms(@movie.platform)
   end
 
   def swipe
@@ -67,7 +77,7 @@ class MoviesController < ApplicationController
     HTTParty.get(
       "https://api.themoviedb.org/3/#{path}",
       headers: {
-        "Authorization" => "Bearer #{ENV["TMDB_API_TOKEN"]}",
+        "Authorization" => "Bearer #{ENV.fetch('TMDB_API_TOKEN', nil)}",
         "Content-Type" => "application/json"
       },
       query: query
@@ -79,21 +89,84 @@ class MoviesController < ApplicationController
     (response["results"] || []).reject { |r| seen_ids.include?(r["id"]) }
   end
 
-  def upsert_movie(result)
-    Movie.find_or_create_by!(tmdb_id: result["id"]) do |movie|
-      movie.title     = result["title"] || result["name"]
-      movie.synopsis  = result["overview"]
-      movie.year      = (result["release_date"] || result["first_air_date"])&.split("-")&.first&.to_i
-      movie.rating    = (result["vote_average"].to_f / 2).round(1)
-      movie.poster_url = "https://image.tmdb.org/t/p/w500#{result["poster_path"]}"
-      movie.category  = params[:genre]
-      movie.platform  = "TMDB"
-    end
+  def upsert_movie(result, type)
+    movie = Movie.find_or_initialize_by(tmdb_id: result["id"])
+
+    movie.assign_attributes(
+      title: result["title"] || result["name"],
+      synopsis: result["overview"],
+      year: (result["release_date"] || result["first_air_date"])&.split("-")&.first&.to_i,
+      rating: (result["vote_average"].to_f / 2).round(1),
+      poster_url: result["poster_path"].present? ? "https://image.tmdb.org/t/p/w500#{result['poster_path']}" : nil,
+      category: params[:genre].presence || "Suggestion"
+    )
+
+    enrich_movie_from_tmdb!(movie, type: type)
+    movie.save!
+    movie
+  end
+
+  def enrich_movie_from_tmdb!(movie, type: "movie")
+    details = tmdb_get(
+      "#{type}/#{movie.tmdb_id}",
+      language: "fr-FR",
+      append_to_response: "credits,videos"
+    )
+
+    watch_providers = tmdb_get("#{type}/#{movie.tmdb_id}/watch/providers")
+
+    movie.actors  = extract_top_actors(details)
+    movie.trailer = extract_trailer_key(details)
+    movie.platform = extract_french_platforms(watch_providers)
+
+    return unless type == "movie" && details["runtime"].present?
+
+    movie.duration = details["runtime"]
+  end
+
+  def extract_top_actors(details)
+    cast = details.dig("credits", "cast") || []
+    cast.first(5).map { |actor| actor["name"] }.join(", ")
+  end
+
+  def extract_trailer_key(details)
+    videos = details.dig("videos", "results") || []
+
+    trailer =
+      videos.find { |video| video["site"] == "YouTube" && video["type"] == "Trailer" && video["official"] == true } ||
+      videos.find { |video| video["site"] == "YouTube" && video["type"] == "Trailer" } ||
+      videos.find { |video| video["site"] == "YouTube" && video["type"] == "Teaser" }
+
+    trailer&.dig("key")
+  end
+
+  def extract_french_platforms(watch_providers)
+    france = watch_providers.dig("results", "FR")
+    return nil if france.blank?
+
+    providers = Array(france["flatrate"])
+    return nil if providers.blank?
+
+    providers.map { |provider| provider["provider_name"] }.uniq.join(", ")
+  end
+
+  def parse_actors(actors_string)
+    return [] if actors_string.blank?
+
+    actors_string.split(",").map(&:strip).first(5)
+  end
+
+  def parse_platforms(platform_string)
+    return [] if platform_string.blank?
+
+    platform_string.split(",").map(&:strip)
   end
 
   def user_platforms
+    return "" unless current_user
+
     PLATFORM_IDS
-      .select { |platform, _| current_user.send(platform) }
+      .select { |platform, _| current_user.public_send(platform) }
       .values
       .join("|")
   end
@@ -125,7 +198,7 @@ class MoviesController < ApplicationController
       "Thriller" => 53,
       "Science Fiction" => 878,
       "Horreur" => 27,
-      "Romance" => 10749,
+      "Romance" => 10_749,
       "Fantasy" => 14
     }[genre]
   end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,85 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
+
   def home
-    @top_week = Movie.order(rating: :desc).limit(10)
-    @top_personal = Movie.joins(:historics).where(historics: { user_id: current_user.id }).order(rating: :desc).limit(10)
+    @top_day = fetch_trending_day_movies
+  end
+
+  private
+
+  def tmdb_get(path, query = {})
+    HTTParty.get(
+      "https://api.themoviedb.org/3/#{path}",
+      headers: {
+        "Authorization" => "Bearer #{ENV.fetch('TMDB_API_TOKEN', nil)}",
+        "Content-Type" => "application/json"
+      },
+      query: query
+    )
+  end
+
+  def fetch_trending_day_movies
+    response = tmdb_get("trending/movie/day", language: "fr-FR")
+    results = response["results"] || []
+
+    results.first(10).map { |result| upsert_movie(result) }
+  end
+
+  def upsert_movie(result)
+    movie = Movie.find_or_initialize_by(tmdb_id: result["id"])
+
+    movie.assign_attributes(
+      title: result["title"],
+      synopsis: result["overview"],
+      year: result["release_date"]&.split("-")&.first&.to_i,
+      rating: (result["vote_average"].to_f / 2).round(1),
+      poster_url: result["poster_path"].present? ? "https://image.tmdb.org/t/p/w500#{result['poster_path']}" : nil,
+      category: "Tendance du jour"
+    )
+
+    enrich_movie_from_tmdb!(movie)
+    movie.save!
+    movie
+  end
+
+  def enrich_movie_from_tmdb!(movie)
+    details = tmdb_get(
+      "movie/#{movie.tmdb_id}",
+      language: "fr-FR",
+      append_to_response: "credits,videos"
+    )
+
+    watch_providers = tmdb_get("movie/#{movie.tmdb_id}/watch/providers")
+
+    movie.actors   = extract_top_actors(details)
+    movie.trailer  = extract_trailer_key(details)
+    movie.duration = details["runtime"] if details["runtime"].present?
+    movie.platform = extract_french_platforms(watch_providers)
+  end
+
+  def extract_top_actors(details)
+    cast = details.dig("credits", "cast") || []
+    cast.first(5).map { |actor| actor["name"] }.join(", ")
+  end
+
+  def extract_trailer_key(details)
+    videos = details.dig("videos", "results") || []
+
+    trailer =
+      videos.find { |video| video["site"] == "YouTube" && video["type"] == "Trailer" && video["official"] == true } ||
+      videos.find { |video| video["site"] == "YouTube" && video["type"] == "Trailer" } ||
+      videos.find { |video| video["site"] == "YouTube" && video["type"] == "Teaser" }
+
+    trailer&.dig("key")
+  end
+
+  def extract_french_platforms(watch_providers)
+    france = watch_providers.dig("results", "FR")
+    return nil if france.blank?
+
+    providers = Array(france["flatrate"])
+    return nil if providers.blank?
+
+    providers.map { |provider| provider["provider_name"] }.uniq.join(", ")
   end
 end
