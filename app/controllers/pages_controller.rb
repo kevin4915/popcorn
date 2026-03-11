efff8fdae5c7bed70401ba7918b81d534ad880d7
guillaume.delattre.it@gmail.com <guillaume.delattre.it@gmail.com>
2026-03-11 15:42:19 +0100

    Homepage OK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
-
+  before_action :authenticate_user!, only: [:home]
   def home
     @top_day = fetch_trending_day_movies
   end
