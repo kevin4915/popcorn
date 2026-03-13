c5aae04894267d3f656ba419299938de89aad5fb
Kévin Delassat <kevin.delassat@gmail.com>
2026-03-13 14:06:02 +0100

    decroissant

--- app/controllers/historics_controller.rb
+++ app/controllers/historics_controller.rb
@@ -1,10 +1,10 @@
 class HistoricsController < ApplicationController
   def films
-    @movies = current_user.movies.where(media_type: "movie")
+    @movies = current_user.movies.where(media_type: "movie").order("historics.created_at DESC")
   end
 
   def series
-    @movies = current_user.movies.where(media_type: "tv")
+    @movies = current_user.movies.where(media_type: "tv").order("historics.created_at DESC")
   end
 
   def destroy
