a41e691ba901be9451967ed63d8277c0ae3f145b
Kévin Delassat <kevin.delassat@gmail.com>
2026-03-13 11:12:31 +0100

    error ok

---
+++ app/javascript/controllers/watchlist_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["btn", "icon"]
+  static values  = { added: Boolean }
+
+  toggle() {
+    const movieId = this.btnTarget.dataset.movieId
+    const method  = this.addedValue ? "DELETE" : "POST"
+    const url     = this.addedValue
+      ? `/movies/${movieId}/remove_from_list`
+      : `/movies/${movieId}/add_to_list`
+
+    fetch(url, {
+      method,
+      headers: {
+        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      if (response.ok) {
+        this.addedValue = !this.addedValue
+        this.iconTarget.className = `fa-solid ${this.addedValue ? "fa-check" : "fa-heart"}`
+        this.btnTarget.className  = `btn-historic-action ${this.addedValue ? "btn-added" : "btn-add-list"}`
+      }
+    })
+  }
+}
