cf307725b4894ff9501c77806a6516c107e52f1b
guillaume.delattre.it@gmail.com <guillaume.delattre.it@gmail.com>
2026-03-16 15:32:01 +0100

    Migration username add

--- db/migrate/20260316143059_add_username_to_users2.rb
+++ db/migrate/20260316143059_add_username_to_users2.rb
@@ -1,4 +1,4 @@
-class AddUsernameToUsers < ActiveRecord::Migration[8.1]
+class AddUsernameToUsers2 < ActiveRecord::Migration[8.1]
   def change
     add_column :users, :username, :string
     add_index :users, :username, unique: true
