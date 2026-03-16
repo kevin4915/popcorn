6ec59604863ed6186dfa615c44b286868199df48
antoine rodrigues <antoine2000r@hotmail.fr>
2026-03-16 14:34:34 +0100

    amiscommu

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @pending_requests = current_user.received_friendships.where(status: 'pending') if @user == current_user
   end
 end
